afda44e947ec6003bbf17732ba76c545b042fc1d
Harry Heckmann <harry@Harrys-MacBook-Pro.local>
2019-01-11 15:12:45 -0600

    adds completed (and fixed)app-2 and app-3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,20 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state ={
+      array: ['pizza', 'steak', 'tacos', 'ice cream'],
+      // filteredArray: [],
+      userInput: ''
+    }
+  }
+  onChange(e){
+    this.setState({userInput: e})
+
+
+    // this.setState({array: ['pizza', 'steak', 'tacos', 'ice cream']})
+    // console.log(this.state.array)
+    // let input = e.target.value
+    // let filtered =this.state.array.filter(function(val){
+    //   return val.includes(input)
+    // })
+    // console.log(filtered)
+    // console.log(input)
+    // this.setState({filteredArray: filtered})
+  }
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+    let displayArray = this.state.array.filter((e, i) => {return e.includes(this.state.userInput)}).map((e,i) => {return <h2 key={i}>{e}</h2>})
+      return (
+        <div className="App">
+          <input 
+            placeholder='Filter List'
+            onChange={(e) => this.onChange(e.target.value)}
+          />
+          {displayArray}
+          {/* <h2>{this.state.filteredArray[0]}</h2>
+          <h2>{this.state.filteredArray[1]}</h2>
+          <h2>{this.state.filteredArray[2]}</h2>
+          <h2>{this.state.filteredArray[3]}</h2> */}
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      );
   }
 }
 
