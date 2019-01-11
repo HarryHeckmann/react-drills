16a1eac9b0c6aaae8d7d7025df875e1e2b4591ec
Harry Heckmann <harry@Harrys-MacBook-Pro.local>
2019-01-11 15:57:34 -0600

    adds completed app-5

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Image from './Image'
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      picture: 'https://cms.splendidtable.org/sites/default/files/styles/w2000/public/Easy_Steak_Frites-LEDE.jpg?itok=IdQsXmBP'
+    }
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Image image={this.state.picture}/>
       </div>
     );
   }
